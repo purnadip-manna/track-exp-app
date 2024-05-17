7b511421163125785eb6d9e870f85073b1296750
Purnadip Manna <purnadipmanna2702@gmail.com>
2024-05-17 21:30:34 +0530

    Category structure upgraded

--- src/app/types/Category.ts
+++ src/app/types/Category.ts
@@ -1,6 +1,7 @@
 export default interface Category {
   id: string;
   name: string;
+  icon: string;
   userId: string;
   modifiedAt: Date;
 }
--- src/app/ui-components/category-form/category-form.component.ts
+++ src/app/ui-components/category-form/category-form.component.ts
@@ -1,8 +1,13 @@
 import { Component, Input, OnInit } from '@angular/core';
 import Category from '../../types/Category';
-import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
 import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
-import {COMMA, ENTER} from '@angular/cdk/keycodes';
+import { COMMA, ENTER } from '@angular/cdk/keycodes';
 import { MatChipsModule } from '@angular/material/chips';
 import { MatFormFieldModule, MatLabel } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
@@ -25,7 +30,7 @@ import { MatIcon } from '@angular/material/icon';
     MatDatepickerModule,
     MatButtonModule,
     MatChipsModule,
-    MatIcon
+    MatIcon,
   ],
   templateUrl: './category-form.component.html',
   styleUrl: './category-form.component.scss',
@@ -41,9 +46,11 @@ export class CategoryFormComponent implements OnInit {
 
   public maxDate = new Date();
   categoryForm: FormGroup = new FormGroup({
-    name: new FormControl(''),
+    name: new FormControl('')
   });
-  constructor(public dialogRef: MatDialogRef<CategoryFormComponent>) {}
+  constructor(
+    public dialogRef: MatDialogRef<CategoryFormComponent>
+  ) {}
 
   ngOnInit(): void {
     if (this.fromParent) {
